2c38614a38469542e02a17b713d156ba8e62c0a2
Zdyrski <zdyrski@student.agh.edu.pl>
2022-11-22 01:00:46 +0100

    final touch

--- backend/itjobboard/src/main/java/com/mzdyrski/itjobboard/services/OfferService.java
+++ backend/itjobboard/src/main/java/com/mzdyrski/itjobboard/services/OfferService.java
@@ -1,7 +1,182 @@
-package com.mzdyrski.itjobboard.service;
+package com.mzdyrski.itjobboard.services;
 
-import com.mzdyrski.itjobboard.domain.Offer;
+import com.mzdyrski.itjobboard.dataTemplates.*;
+import com.mzdyrski.itjobboard.domain.*;
+import com.mzdyrski.itjobboard.exceptions.OfferNotAvailableException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.stereotype.Service;
 
-public interface OfferService {
-    Offer addOffer();
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.*;
+
+import static com.mzdyrski.itjobboard.enums.EmailType.*;
+import static com.mzdyrski.itjobboard.enums.Role.*;
+
+@RequiredArgsConstructor
+@Service
+public class OfferServiceImpl {
+
+    final private OfferRepository offerRepository;
+    final private ApplicationRepository applicationRepository;
+    final private UserRepository userRepository;
+
+    final private EmployeesCvRepository cvRepository;
+    final private EmailService emailService;
+    final private MongoTemplate mongoTemplate;
+
+    public OfferDetailedData getOfferDetails(String offerId, User user) throws OfferNotAvailableException {
+        var offer = offerRepository.findById(offerId).orElseThrow();
+        var employer = (Employer) userRepository.findById(offer.getEmployerId()).orElseThrow();
+        if ((offer.getApprovalStatus() == 1 && !offer.isArchived()) ||
+                (user != null && (Objects.equals(user.getId(), employer.getId()) || Objects.equals(user.getRole(), ROLE_ADMIN.name())))) {
+            return new OfferDetailedData(
+                    offer.getTitle(),
+                    employer.getCompanyName(),
+                    employer.getCompanySize(),
+                    employer.getCompanySiteUrl(),
+                    employer.getCompanyLogoUrl(),
+                    offer.getAddress().city(),
+                    offer.getRemote(),
+                    offer.getExperienceLevel(),
+                    offer.getDate(),
+                    offer.getTechStack(),
+                    offer.getContracts(),
+                    offer.getDescription()
+            );
+        }else {
+            throw new OfferNotAvailableException("Offer not available");
+        }
+    }
+
+    public List<ListElOfferData> getOffersByFilters(Aggregation aggregation) {
+        //TODO applying possible filters and connect with employer img src
+        var offers = mongoTemplate.aggregate(aggregation, "offers", Offer.class);
+        var result = new ArrayList<ListElOfferData>();
+        for (Offer offer : offers) {
+            result.add(getListElForGivenOffer(offer));
+        }
+        return result;
+    }
+
+    public List<ListAdminElOfferData> getOffersByAdminFilters(Aggregation aggregation) {
+        //TODO applying possible filters and connect with employer img src
+        var offers = mongoTemplate.aggregate(aggregation, "offers", Offer.class);
+        var result = new ArrayList<ListAdminElOfferData>();
+        for (Offer offer : offers) {
+            result.add(getListAdminElForGivenOffer(offer));
+        }
+        return result;
+    }
+
+    public List<ListElOfferData> getOffersByUser(User user) {
+        var result = new ArrayList<ListElOfferData>();
+        if (Objects.equals(user.getRole(), ROLE_EMPLOYER.name())) {
+            var offers = offerRepository.findAllByEmployerIdOrderByDateDesc(user.getId());
+            for (Offer offer : offers) {
+                result.add(getListElForGivenOffer(offer));
+            }
+        } else if (Objects.equals(user.getRole(), ROLE_EMPLOYEE.name())) {
+            var applications = applicationRepository.findApplicationsByUserId(user.getId());
+            for (Application application : applications) {
+                var offer = offerRepository.findById(application.getOfferId()).orElseThrow();
+                result.add(getListElForGivenOffer(offer));
+            }
+        }
+        return result;
+    }
+
+    public void addOffer(User employer, OfferData offerData) throws MessagingException, IOException {
+        if (!Objects.equals(employer.getRole(), ROLE_EMPLOYER.name())) {
+            return;
+        }
+        var newOffer = new Offer();
+        newOffer.setEmployerId(employer.getId());
+        newOffer.setTitle(offerData.title());
+        newOffer.setAddress(offerData.address());
+        newOffer.setRemote(offerData.remote());
+        newOffer.setContracts(offerData.contracts());
+        newOffer.setSalaryShort(getShortSalary(offerData.contracts()));
+        newOffer.setExperienceLevel(offerData.experienceLevel());
+        var reverseSortedTechStack = Arrays.stream(offerData.techStack()).sorted(Comparator.comparing(SkillData::level).reversed()).toArray(SkillData[]::new);
+        newOffer.setTechStack(reverseSortedTechStack);
+        newOffer.setTags(getTags(reverseSortedTechStack));
+        newOffer.setDate(new Date());
+        newOffer.setDescription(offerData.description());
+        newOffer.setApprovalStatus(0);
+        offerRepository.save(newOffer);
+        emailService.sendEmail(employer.getEmail(), OFFER_ADDED, "url");
+    }
+
+    public void setOfferStatus(User admin, String offerId, OfferStatusData data) {
+        if (!Objects.equals(admin.getRole(), ROLE_ADMIN.name())) {
+            return;
+        }
+        var offer = offerRepository.findById(offerId).orElseThrow();
+        offer.setApprovalStatus(data.approvalState());
+        offer.setArchived(data.archived());
+        offerRepository.save(offer);
+    }
+
+    private ListElOfferData getListElForGivenOffer(Offer offer) {
+        var employerInfo = (Employer) userRepository.findById(offer.getEmployerId()).orElseThrow();
+        return new ListElOfferData(
+                offer.getId(),
+                offer.getTitle(),
+                employerInfo.getCompanyName(),
+                employerInfo.getCompanyLogoUrl(),
+                offer.getAddress().city(),
+                offer.getRemote(),
+                offer.getTags(),
+                offer.getSalaryShort(),
+                offer.getDate()
+        );
+    }
+
+    private ListAdminElOfferData getListAdminElForGivenOffer(Offer offer) {
+        var employerInfo = (Employer) userRepository.findById(offer.getEmployerId()).orElseThrow();
+        return new ListAdminElOfferData(
+                offer.getId(),
+                offer.getTitle(),
+                employerInfo.getCompanyName(),
+                employerInfo.getCompanyLogoUrl(),
+                offer.getAddress().city(),
+                offer.getRemote(),
+                offer.getTags(),
+                offer.getSalaryShort(),
+                offer.getDate(),
+                offer.getApprovalStatus(),
+                offer.isArchived()
+        );
+    }
+
+    private String getShortSalary(ContractData[] contracts) {
+        var minMoney = 0;
+        var maxMoney = 0;
+        for (ContractData contract : contracts) {
+            if (!contract.undisclosed()) {
+                if (contract.maxMoney() > maxMoney) {
+                    minMoney = contract.minMoney();
+                    maxMoney = contract.maxMoney();
+                }
+            }
+        }
+        if (maxMoney != 0) {
+            return String.format("%s - %s PLN", minMoney, maxMoney);
+        }
+        return "Undisclosed";
+    }
+
+    private String[] getTags(SkillData[] reverseSortedTechStack) {
+        var result = new ArrayList<String>();
+        for (int i = 0; i < reverseSortedTechStack.length; i++) {
+            if (i == 3) {
+                break;
+            }
+            result.add(reverseSortedTechStack[i].skillName());
+        }
+        return result.toArray(String[]::new);
+    }
 }
--- backend/itjobboard/src/main/java/com/mzdyrski/itjobboard/services/UserService.java
+++ backend/itjobboard/src/main/java/com/mzdyrski/itjobboard/services/UserService.java
@@ -1,24 +1,187 @@
-package com.mzdyrski.itjobboard.service;
+package com.mzdyrski.itjobboard.services;
 
 import com.mzdyrski.itjobboard.dataTemplates.ChangePasswordData;
+import com.mzdyrski.itjobboard.dataTemplates.UserStatusData;
 import com.mzdyrski.itjobboard.dataTemplates.UserUpdateData;
-import com.mzdyrski.itjobboard.domain.Employee;
-import com.mzdyrski.itjobboard.domain.User;
+import com.mzdyrski.itjobboard.domain.*;
+import com.mzdyrski.itjobboard.enums.Role;
 import com.mzdyrski.itjobboard.exceptions.BadRequestDataException;
 import com.mzdyrski.itjobboard.exceptions.InvalidEmailException;
 import com.mzdyrski.itjobboard.exceptions.UserExistsException;
-import org.springframework.http.ResponseEntity;
+import com.mzdyrski.itjobboard.security.JWTTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.bson.types.Binary;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
 
-public interface UserService {
+import static com.mzdyrski.itjobboard.constants.SecurityConstants.TOKEN_HEADER;
+import static com.mzdyrski.itjobboard.enums.EmailType.ACCOUNT_CREATED;
+import static com.mzdyrski.itjobboard.enums.Role.ROLE_EMPLOYEE;
+import static com.mzdyrski.itjobboard.enums.Role.ROLE_EMPLOYER;
 
-    void register(String email, String password, String userType, String siteUrl) throws UserExistsException, InvalidEmailException, BadRequestDataException, MessagingException, IOException;
-    User findUserByEmail(String email);
-    void updateUserInfo(User user, UserUpdateData data);
-    void changePassword(String authorizationHeader, ChangePasswordData data) throws BadRequestDataException;
-    void resetPassword(String email);
-    void updateEmployeeCv(Employee employee, MultipartFile file) throws IOException;
+@RequiredArgsConstructor
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final ConfirmationTokenRepository tokenRepository;
+    private final EmployeesCvRepository cvRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final LoginAttemptService loginAttemptService;
+    private final JWTTokenProvider jwtTokenProvider;
+    private final MongoTemplate mongoTemplate;
+    private final EmailService emailService;
+
+    @Override
+    public UserSecurity loadUserByUsername(String email) {
+        var user = findUserByEmail(email);
+        validateLoginAttempt(user);
+        return new UserSecurity(user);
+    }
+
+    @Override
+    public void register(String email, String password, String role, String siteUrl) throws UserExistsException, InvalidEmailException, BadRequestDataException, MessagingException {
+        validateNewEmail(email);
+        var user = getUserClass(role);
+        user.setEmail(email);
+        user.setPassword(encodedPassword(password));
+        user.setRole(Role.valueOf(role).name());
+        user.setAuthorities(Role.valueOf(role).getAuthorities());
+        user.setActive(false);
+        user.setJoinDate(new Date());
+        user.setLocked(false);
+        userRepository.save(user);
+        var confirmationToken = new ConfirmationToken(user.getId());
+        tokenRepository.save(confirmationToken);
+        emailService.sendEmail(email, ACCOUNT_CREATED, siteUrl, confirmationToken.getToken());
+    }
+
+    public void activateUser(String token) {
+        var confirmationToken = tokenRepository.findConfirmationTokenByToken(token).orElseThrow();
+        var user = userRepository.findById(confirmationToken.getUserId()).orElseThrow();
+        user.setActive(true);
+        userRepository.save(user);
+        tokenRepository.delete(confirmationToken);
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+    @Override
+    public void updateUserInfo(User user, UserUpdateData data) {
+        if (Objects.equals(user.getRole(), ROLE_EMPLOYEE.name())) {
+            var employee = (Employee) user;
+            employee.setFirstName(data.firstName());
+            employee.setLastName(data.lastName());
+            userRepository.save(employee);
+        } else if (Objects.equals(user.getRole(), ROLE_EMPLOYER.name())) {
+            var employer = (Employer) user;
+            employer.setCompanyName(data.companyName());
+            employer.setCompanySize(data.companySize());
+            employer.setCompanyLogoUrl(data.companyLogoUrl());
+            employer.setCompanySiteUrl(data.companySiteUrl());
+            userRepository.save(employer);
+        }
+    }
+
+    @Override
+    public void changePassword(String authorizationHeader, ChangePasswordData data) throws BadRequestDataException {
+        var user = getUserFromTokenHeader(authorizationHeader);
+        if (user != null) {
+            if (passwordEncoder.matches(data.oldPassword(), user.getPassword())) {
+                var encodedNew = encodedPassword(data.newPassword());
+                user.setPassword(encodedNew);
+                userRepository.save(user);
+            }else {
+                throw new BadRequestDataException("Wrong password");
+            }
+        }
+    }
+
+    @Override
+    public void resetPassword(String email) {
+        var user = findUserByEmail(email);
+        var newPassword = RandomStringUtils.randomAlphanumeric(9);
+        var encodedNewPassword = passwordEncoder.encode(newPassword);
+        user.setPassword(encodedNewPassword);
+        // TODO send email
+        userRepository.save(user);
+    }
+
+    @Override
+    public void updateEmployeeCv(Employee employee, MultipartFile file) throws IOException {
+        if (!Objects.equals(employee.getRole(), ROLE_EMPLOYEE.name())) {
+            return;
+        }
+        var cv = new EmployeesCv();
+        cv.setEmployeeId(employee.getId());
+        cv.setFilename(file.getOriginalFilename());
+        cv.setFileType(file.getContentType());
+        cv.setFileSize(file.getSize());
+        cv.setFile(new Binary(file.getBytes()));
+        cvRepository.save(cv);
+    }
+
+    public List<User> getUsersByFilters(Aggregation aggregation) {
+        return mongoTemplate.aggregate(aggregation, "users", User.class).getMappedResults();
+    }
+
+    public void updateUserStatus(String userId, UserStatusData data) {
+        var user = userRepository.findById(userId).orElseThrow();
+        user.setLocked(data.locked());
+        user.setActive(data.active());
+        userRepository.save(user);
+    }
+
+    public User getUserFromTokenHeader(String authorizationHeader) {
+        var token = StringUtils.remove(authorizationHeader, TOKEN_HEADER);
+        var email = jwtTokenProvider.getSubject(token);
+        return findUserByEmail(email);
+    }
+
+    private void validateLoginAttempt(User user) {
+        if (!user.isLocked()) {
+            user.setLocked(loginAttemptService.hasExceededMaxAttempts(user.getEmail()));
+        } else {
+            loginAttemptService.evictUserFromCache(user.getEmail());
+        }
+    }
+
+    private User getUserClass(String role) throws BadRequestDataException {
+        return switch (role) {
+            case "ROLE_EMPLOYEE" -> new Employee();
+            case "ROLE_EMPLOYER" -> new Employer();
+            case "ROLE_ADMIN" -> new User();
+            default -> throw new BadRequestDataException("Unsupported role");
+        };
+    }
+
+    private String encodedPassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    private void validateNewEmail(String email) throws UserExistsException, InvalidEmailException {
+        if (StringUtils.isNotBlank(email)) {
+            if (userRepository.findUserByEmail(email) != null) {
+                throw new UserExistsException("User by given email exists");
+            }
+        } else {
+            throw new InvalidEmailException("Invalid Email");
+        }
+    }
+
 }
